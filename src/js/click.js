c5f6156aae191c27554697056d00f04f7df6959e
aaronfraint <aaronfraint@icloud.com>
2021-12-13 16:55:57 -0500

    add spinner

--- src/js/click.js
+++ src/js/click.js
@@ -1,4 +1,5 @@
 import { API_ROOT } from "./api";
+import { Spinner } from "spin.js";
 let SELECTED_TAZS = [];
 
 const button_to_select_tazs = document.getElementById("button-to-select-tazs");
@@ -11,7 +12,8 @@ const return_to_initial_state = (map) => {
   button_to_select_tazs.classList.remove("selected-button");
   button_to_clear_tazs.style.setProperty("display", "none");
   button_to_analyze_tazs.style.setProperty("display", "none");
-  map.setFilter("destinations", ["in", "tazt", "-1"]);
+  map.setPaintProperty("destinations", "fill-opacity", 0);
+  map.setFilter("selected-taz", ["in", "tazt", ...SELECTED_TAZS]);
 };
 
 const button_logic = (map) => {
@@ -27,11 +29,33 @@ const button_logic = (map) => {
 
   button_to_clear_tazs.onclick = () => {
     SELECTED_TAZS = [];
-    map.setFilter("selected-taz", ["in", "tazt", "-1"]);
+    map.setPaintProperty("destinations", "fill-opacity", 0);
     return_to_initial_state(map);
   };
 
   button_to_analyze_tazs.onclick = () => {
+    var opts = {
+      lines: 13, // The number of lines to draw
+      length: 38, // The length of each line
+      width: 17, // The line thickness
+      radius: 45, // The radius of the inner circle
+      scale: 1, // Scales overall size of the spinner
+      corners: 1, // Corner roundness (0..1)
+      speed: 1, // Rounds per second
+      rotate: 0, // The rotation offset
+      animation: "spinner-line-fade-quick", // The CSS animation name for the lines
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: "#ffffff", // CSS color or array of colors
+      fadeColor: "transparent", // CSS color or array of colors
+      top: "50%", // Top position relative to parent
+      left: "50%", // Left position relative to parent
+      shadow: "0 0 1px transparent", // Box-shadow for the lines
+      zIndex: 2000000000, // The z-index (defaults to 2e9)
+      className: "spinner", // The CSS class to assign to the spinner
+      position: "absolute", // Element positioning
+    };
+    var target = document.getElementById("foo");
+    var spinner = new Spinner(opts).spin(target);
     let api_path = API_ROOT + "/flows/?q=" + SELECTED_TAZS.join("&q=");
 
     var request = new XMLHttpRequest();
@@ -42,6 +66,8 @@ const button_logic = (map) => {
         var json = JSON.parse(this.response);
         map.getSource("destination-geojson").setData(json);
         console.log("Updated");
+        map.setPaintProperty("destinations", "fill-opacity", 0.7);
+        spinner.stop();
       }
     };
     request.send();
