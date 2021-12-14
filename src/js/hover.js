57dd3f58b3146efd13c51ade738ca8b69ef3b5bf
aaronfraint <aaronfraint@icloud.com>
2021-12-14 08:58:30 -0500

    add popup

--- src/js/hover.js
+++ src/js/hover.js
@@ -1,3 +1,5 @@
+import { add_popup_to_map, clear_popups } from "./popups";
+
 const use_pointer_when_hovering = (map, layername) => {
   /**
    * For the provided layername, set the cursor to use a
@@ -21,6 +23,24 @@ const use_pointer_when_hovering = (map, layername) => {
   });
 };
 
+const add_popup_to_destination = (map) => {
+  map.on("mousemove", "destinations", (e) => {
+    clear_popups();
+    let msg =
+      "<h1> TAZ #" +
+      e.features[0].properties.tazt +
+      "</h1><hr><ul><li>" +
+      e.features[0].properties.total_trips +
+      " trips</li><li>" +
+      e.features[0].properties.trip_density +
+      " density</li></ul>";
+    add_popup_to_map(map, msg, e);
+  });
+  map.on("mouseleave", "destinations", () => {
+    clear_popups();
+  });
+};
+
 const wire_mouse_hover = (map) => {
   /**
    * Show interactivity tooltip hints for all layers defined within
@@ -30,6 +50,8 @@ const wire_mouse_hover = (map) => {
   var layers = ["taz-fill"];
 
   layers.forEach((lyr) => use_pointer_when_hovering(map, lyr));
+
+  add_popup_to_destination(map);
 };
 
 export { wire_mouse_hover };
