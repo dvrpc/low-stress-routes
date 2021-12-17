a325fd374a4993532bea29edcca13dcb12d0a842
aaronfraint <aaronfraint@icloud.com>
2021-12-17 16:35:17 -0500

    update layers

--- src/js/layers.js
+++ src/js/layers.js
@@ -1,81 +1,52 @@
 const map_layers = {
-  destinations: {
-    id: "destinations",
-    type: "fill",
-    source: "destination-geojson",
-    layout: {},
-    paint: {
-      "fill-opacity": 0.7,
-      "fill-color": {
-        property: "trip_density",
-        default: "white",
-        stops: [
-          [0, "#edf8fb"],
-          [0.0000000003, "#b3cde3"],
-          [0.000000003, "#8c96c6"],
-          [0.0000003003, "#88419d"],
-          [0.00001, "black"],
-        ],
-      },
-    },
-  },
-
-  "taz-fill": {
-    id: "taz-fill",
-    type: "fill",
-    source: "taz-tiles",
-    "source-layer": "TAZ_2010",
-    layout: {},
-    paint: {
-      "fill-opacity": 0,
-      "fill-color": "black",
-    },
-  },
-  "taz-outline": {
-    id: "taz-outline",
-    type: "line",
-    source: "taz-tiles",
-    "source-layer": "TAZ_2010",
-    layout: {},
-    paint: {
-      "line-width": 0.8,
-      "line-opacity": 0.2,
-      "line-color": "black",
-    },
-  },
-  "selected-taz": {
-    id: "selected-taz",
-    type: "line",
-    source: "taz-tiles",
-    "source-layer": "TAZ_2010",
-    layout: {},
-    paint: {
-      "line-opacity": 0.8,
-      "line-width": 2,
-      "line-color": "black",
-    },
-    filter: ["==", "tazt", "-1"],
-  },
-  "rr-lines": {
-    id: "rr-lines",
-    type: "line",
-    source: "rr-lines-src",
-    layout: {},
-    paint: {
-      "line-width": 4,
-      "line-opacity": 0.4,
-      "line-color": "blue",
-    },
-  },
-  "rr-stops": {
-    id: "rr-stops",
+  nodes: {
+    id: "nodes",
     type: "circle",
-    source: "rr-stops-src",
+    source: "node-tiles",
+    "source-layer": "lts_nodes",
     layout: {},
     paint: {
-      "circle-radius": 6,
+      "circle-opacity": 0.7,
+      "circle-color": "pink",
+      "circle-radius": 3,
+    },
+    minzoom: 9.5,
+  },
+  start: {
+    id: "start",
+    type: "circle",
+    source: "node-tiles",
+    "source-layer": "lts_nodes",
+    layout: {},
+    paint: {
+      "circle-opacity": 0.7,
+      "circle-color": "green",
+      "circle-radius": 10,
+    },
+    filter: ["==", "nodeid", "-1"],
+  },
+  end: {
+    id: "end",
+    type: "circle",
+    source: "node-tiles",
+    "source-layer": "lts_nodes",
+    layout: {},
+    paint: {
       "circle-opacity": 0.7,
       "circle-color": "red",
+      "circle-radius": 10,
+    },
+    filter: ["==", "nodeid", "-1"],
+  },
+  route: {
+    id: "route",
+    type: "line",
+    source: "route-src",
+    layout: {},
+    paint: {
+      "line-opacity": 0.7,
+      "line-color": "green",
+      "line-width": 6,
     },
   },
 };
