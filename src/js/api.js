3d02723dd27a6d836ecaa2dc2c9d7480a6392295
aaronfraint <aaronfraint@icloud.com>
2022-01-11 09:53:36 -0500

    update path

--- src/js/api.js
+++ src/js/api.js
@@ -1,4 +1,5 @@
 const API_ROOT =
-  "https://low-stress-bike-routing-ocy3k.ondigitalocean.app/api/lowstress";
+  // "https://low-stress-bike-routing-ocy3k.ondigitalocean.app/api/lowstress";
+  "https://low-stress-routes-api.planninglab.org/api/lowstress";
 
 export { API_ROOT };
